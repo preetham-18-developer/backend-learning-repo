1103d5363fa5a82484c547435be88405cc07b804
preetham-18-developer <preethamgoud2006@gmail.com>
2026-04-09 21:58:24 +0530

    Add Day 1 and Day 2 independently as separate folders

---
+++ day2/index.js
@@ -0,0 +1,25 @@
+﻿import express from "express";
+
+const app = express();
+const port = 3000;
+
+app.get("/" , (req ,res) => {
+    res.send("<h1> heyy hii </h1>");
+})
+
+app.post("/about" ,(req,res)=>{
+    res.send("<p> This is Post Method </P>");
+})
+app.put("/about" ,(req,res)=>{
+    res.send("<p>Before it is Post now  This is put Method </P>");
+})
+app.patch("/about" ,(req,res)=>{
+    res.send("<p> This is patch Method </P>");
+})
+app.delete("/about" ,(req,res)=>{
+    res.send("<p> This is Post Method </P>");
+})
+
+app.listen( port , () => {
+    console.log("heyy Backend Server running");
+});
